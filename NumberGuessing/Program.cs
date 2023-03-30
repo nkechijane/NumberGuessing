37208bbbbc35501d81af43a2d387462ca9f6de69
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-03-30 11:39:00 +0100

    Initial Commit

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + num);
+            
+            //Declare a variable first name and assign a value to it, using var as data type
+            var firstName = "Jane";
+            Console.WriteLine(firstName);
+            
+            //Declare an integer variable without assigning a value
+            int number;
+            
+            //Declare a variable that accepts true or false and assign a value to it using the var data type
+            var isNumber = true;
+            Console.WriteLine(isNumber);
+            
+            
+            
+        }
+    }
+}
